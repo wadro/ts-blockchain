f164cd7ad791cd2f373079bbea72cf244a7c3a80
wadro <wadro@projfrom.me>
2021-04-26 23:40:58 +0900

    interface ?(optional) test

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ console.log("run index.ts");
 interface Human {
   name: string;
   age: number;
+  id?: number;
 }
 const person = {
   name: "jj",
-  age: 1
+  age: 1,
+  // id: 1,
 };
 
 const name = "kjh",
   age = 26;
 
 const sayHi = (person:Human):string => {
-  return `hi, ${name}, are you ${age}?`;
+  return `hi, ${person.name}, are you ${person.age}?`;
 }
 console.log(sayHi(person));
 console.log("watch");
