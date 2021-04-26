fe36e25af2413771aa35b2742a4dd5218524855f
wadro <wadro@projfrom.me>
2021-04-27 00:03:13 +0900

    interface and class test

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,36 @@ interface Human {
   age: number;
   id?: number;
 }
+
+class HumanClass{
+  public name: string;
+  public age: number;
+  private id?: number;
+  constructor(name: string,age: number,id?:number){
+    this.name = name;
+    this.age = age;
+    this.id = id;
+  }
+}
+
 const person = {
   name: "jj",
   age: 1,
   // id: 1,
 };
+const lynn = new HumanClass("Lynn",30,2);
 
 const name = "kjh",
   age = 26;
 
-const sayHi = (person:Human):string => {
-  return `hi, ${person.name}, are you ${person.age}?`;
+const sayHi = (humanInf?:Human,humanClass?:HumanClass):string => {
+  if(humanClass != undefined){
+    return `is inf ${humanInf}? hi, ${humanClass.name}, are you ${humanClass.age}?`;
+  }else{
+    return `is class ${humanClass}?  hi, ${humanInf.name}, are you ${humanInf.age}?`;
+  }
 }
+console.log(sayHi(person,lynn));
 console.log(sayHi(person));
 console.log("watch");
 
